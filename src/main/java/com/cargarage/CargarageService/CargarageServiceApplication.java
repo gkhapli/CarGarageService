81c49b01628a5a9801f89f88ca0e55770b1ec3cf
gkhapli <gkhapli@gmail.com>
2021-02-02 20:18:31 +0100

    Initial Commit
    Generated a empty Spring Boot Project from Spring.io with version 2.4.2 and Java 8

---
+++ src/main/java/com/cargarage/CargarageService/CargarageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cargarage.CargarageService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargarageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargarageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cargarage/CargarageService/CargarageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargarage.CargarageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargarageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
